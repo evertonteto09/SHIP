1450d2b9aef71085582d38f3178b954d0a1e79f1
evertonteto09 <everton.teto.zica.deus@gmail.com>
2026-01-16 21:36:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def ship():
         avatar2_img = circle(avatar2_img)
 
         # Colar avatares (ajuste se quiser)
-        base.paste(avatar1_img, (100, 290), avatar1_img)
-        base.paste(avatar2_img, (890, 290), avatar2_img)
+        base.paste(avatar1_img, (80, 290), avatar1_img)
+        base.paste(avatar2_img, (860, 290), avatar2_img)
 
 # ❤️ Selecionar coração
         if percent <= 25:
